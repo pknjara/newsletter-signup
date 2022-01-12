dde5afbe62b49a63e90984d31884108ec61f5b9b
unknown <pradipanjara@gmial.com>
2022-01-12 16:19:14 +0530

    og commit

---
+++ app.js
@@ -0,0 +1,89 @@
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+app.get("/",function(req,res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req,res){
+  const firstName = req.body.fn;
+  const lastName = req.body.ln;
+  const email = req.body.email;
+
+  const data = {
+    members:[
+      {
+        email_address: email,
+        status: "subscribed",
+        merge_fields:{
+          FNAME:firstName,
+          LNAME:lastName
+        }
+
+      }
+    ]
+  }
+
+  const jsonData = JSON.stringify(data);
+
+  const url =  "https://us20.api.mailchimp.com/3.0/lists/863c65f51e";
+
+  const options= {
+    method:"POST",
+    auth: "pradip:7ca183fd69c37abec9da4ec3f1b9b4c7-us20"
+  }
+
+  const request = https.request(url,options,function(response){
+
+    if(response.statusCode===200){
+      res.sendFile(__dirname+'/success.html');
+    }
+    else{
+      res.sendFile(__dirname+'/failure.html');
+    }
+
+
+    response.on("data", function(data){
+      console.log(JSON.parse(data));
+    });
+
+
+  });
+
+  request.write(jsonData);
+  request.end();
+
+
+});
+
+app.post("/failure",function(req,res){
+  res.redirect("/");
+});
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log('server is running on port 3000');
+});
+
+
+
+// api key
+// 7ca183fd69c37abec9da4ec3f1b9b4c7-us20
+
+
+// audience id
+// 863c65f51e
